cecb7742a56a37a4e0fa2ad65372b39321ff6e17
Adiel <Adiel.Ahmad@protonmail.com>
2019-06-08 18:49:06 +0200

    backProp angefangen, mmul der einzelnen Werte

--- haskell.hs
+++ haskell.hs
@@ -5,10 +5,19 @@ import System.IO.Unsafe
 
 data NeuralNetwork = NeuralNetwork { weights::[Matrix Float], biases::[Matrix Float] } deriving Show
 
+
+-- multipliziert jeden wert einer matrix mit dem Wert der anderen Matrix
+mmul :: Matrix Integer -> Matrix Integer -> Matrix Integer
+mmul m n = fromList (length listM) (length listN) (zipWith (*) listM listN)
+           where listM = toList m
+                 listN = toList n
+
 main = do
     let network = initializeNeuralNetwork [2,3,2]
     print network
-    
+    print "-------------------"
+    print (forwardPass network (fromList 2 1[2,1]))
+
 -- initialize <inputCount> <outputCount> <hiddenCount>
 initializeNeuralNetwork :: [Int] -> NeuralNetwork
 initializeNeuralNetwork l = NeuralNetwork 
@@ -22,13 +31,19 @@ zeroMatrix :: Int -> Int -> Matrix Float
 zeroMatrix n m = matrix n m $ \(i,j) -> 0.0
 
 sigmoid x = 0.5 * (1 + tanh (x/2))
+sigmoid' x = sigmoid x * (1-sigmoid x)
 
 forward [] _ activation = []
 forward _ [] activation = []
-forward (w:weights) (b:biases) activation = ergebnis : (forward weights biases ergebnis) where ergebnis = ((multStd w activation))
+forward (w:weights) (b:biases) activation = ergebnis : (forward weights biases ergebnis) where ergebnis = fmap sigmoid (b + (multStd w activation))
 
 forwardTest (w:weights) (b:biases) activation = ergebnis where ergebnis = (multStd w activation)
 
 forwardPass network input = input : forward (weights network) (biases network) input
 
---backProp network input = 
+--backProp [] _ _ error = []
+--backProp _ [] _ error = []
+--backProp _ _ [] error = []
+--backProp (w:r_weights) (b:r_biases) (a:r_activations) error = mmul error (fmap sigmoid' (b + (multStd w a)))
+
+--backPropTest network input output = backProp (reverse $ weights network) (reverse $ biases network) (reverse $ init fp) ( (last fp - output)) where fp = forwardPass network input
