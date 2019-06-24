4ac70e48c9c8da4ea8b6065c60e6f7c1e8df1622
Marco <herzog-marco@gmx.net>
2019-06-24 23:25:46 +0200

    fixed mnist-conversion, can now be used as trainData

--- mnist_test.hs
+++ mnist_test.hs
@@ -2,45 +2,44 @@ import System.IO as IO
 import Data.Matrix
 import Data.ByteString as BS
 import Prelude as P
+import Data.List.Split
 
-
--- FUNCTION TO TEST :
 
 convert_mnist_auto size = mnist_to_trainData "data/train-images" "data/train-labels" size 28 28    -- max size is 60000
 
+
+
+type TrainData = (Matrix Float, Int)
 
 -- String, String -> names of the files to be parsed to TrainData
 -- Int -> number of TrainDatas to be created
 -- Int, Int -> measurements of the images (X*Y)
 
--- mnist_to_trainData :: String -> String -> Int -> Int -> Int -> [TrainData]
+mnist_to_trainData :: String -> String -> Int -> Int -> Int -> IO [TrainData]
 
 mnist_to_trainData imgFile labelFile count imgX imgY = do
-                                              contentLabel <- BS.readFile labelFile
-                                              let labels = P.drop imgDrop $ unpack $ BS.take (imgX * imgY * count + imgDrop) contentLabel
                                               matrixes <- mnist_to_matrixes imgFile count imgX imgY
+                                              labels <- mnist_to_labellist labelFile count
                                               let trainData = P.zip matrixes labels
-                                              --IO.putStrLn $ show trainData
-                                              IO.putStrLn $ show $ P.length trainData
-                                              where imgDrop = 16
+                                              return trainData
 
---mnist_to_matrixes :: String -> Int-> Int-> Int -> [Matrix Float]
+
+mnist_to_matrixes :: String -> Int-> Int-> Int -> IO [Matrix Float]
 
 mnist_to_matrixes fileName count imgX imgY = do
-                                             content <- BS.readFile fileName
-                                             let res = P.drop matDrop $ unpack $ BS.take (imgX * imgY * count + matDrop) content
-                                             let matList = P.map (fromList (imgX*imgY) 1) (part res (imgX * imgY))
+                                             imgContent <- BS.readFile fileName
+                                             let imgs = unpack $ BS.take (imgX * imgY * count) $ BS.drop matDrop imgContent
+                                             let float_imgs = ( P.map fromIntegral imgs) :: [Float]
+                                             let matList = P.map (fromList (imgX*imgY) 1) (chunksOf (imgX * imgY) float_imgs)
                                              return matList
-                                             where matDrop = 8
+                                             where matDrop = 16     -- drops 16 bytes of file info from start of imgFile
 
--- Helpers
+mnist_to_labellist :: String -> Int -> IO [Int]
 
--- Splits List into List of Lists
--- Argument 1: List to split
--- Argument 2: number of elems per List
--- Result: List of Lists (with specified nElems)
+mnist_to_labellist fileName count = do
+                                   labelContent <- BS.readFile fileName
+                                   let labels = unpack $ BS.take count $ BS.drop labelDrop labelContent
+                                   let int_labels = P.map fromIntegral labels
+                                   return int_labels
+                                   where labelDrop = 8 -- drops 8 bytes of file info from start of labelFile
 
-part:: (Num a) => [a] -> Int-> [[a]]
-
-part [] _ = []
-part list nElems = [P.take (nElems) list] ++ part (P.reverse (P.take ((P.length list) - nElems) (P.reverse list)))  nElems
