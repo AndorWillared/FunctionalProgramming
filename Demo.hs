ecc56fadc25ede4d5f851b842ce553c7dfa76eff
Marco <herzog-marco@gmx.net>
2019-07-06 18:09:06 +0200

    test

--- Demo.hs
+++ Demo.hs
@@ -1,6 +1,15 @@
 import NeuralNetwork
 import MNIST
 
+main = do
+        net <- demo
+        putStrLn "Choose image file: "
+        path <- getLine
+        pic <- pngToVector path
+        let res =  mapToResult $ predict net pic
+        putStrLn ("The result is: " ++ show (snd res) ++ "with a confidence of " ++ show (fst res ))
+
+
 demo = do
         let nn = createNeuralNetwork [784, 16, 16, 10]
         trainingSamples <- getTrainingSamples
