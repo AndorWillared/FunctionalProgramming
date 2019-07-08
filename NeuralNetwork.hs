dea3e6e6636cc0d0ae4928bb0e93edb0f84745c0
FelixBruebach <felixbruebach@outlook.de>
2019-07-08 20:22:49 +0200

    hotfix

--- NeuralNetwork.hs
+++ NeuralNetwork.hs
@@ -58,18 +58,18 @@ data BackpropR = BackpropR { nn :: NeuralNetwork , totalError :: Float , totalIt
 backprop :: NeuralNetwork -> Matrix Float -> Matrix Float -> Float -> Float -> Int -> IO (BackpropR)
 backprop nn input output learningRate totalError totalIterations = do
   let err = 0.5 * (sum $ toList (fmap (^2) ((last activations) - output)))
-  let updatedNN = update nn (reverse (gradients (reverse (weights nn)) (reverse (biases nn)) (reverse (init activations)) ((last activations) - output))) learningRate
+  let updatedNN = apply nn (reverse (gradients (reverse (weights nn)) (reverse (biases nn)) (reverse (init activations)) ((last activations) - output))) learningRate
   putStrLn ((show (totalIterations + 1)) ++ ": " ++ show ((totalError + err)/(fromIntegral (totalIterations + 1))))
   return (BackpropR updatedNN (totalError + err) (totalIterations + 1))
   where activations = forwardPass nn input
 
-update :: NeuralNetwork -> [(Matrix Float, Matrix Float)] -> Float -> NeuralNetwork
-update nn updates learningRate = NeuralNetwork (config nn) (update' (weights nn) (fst (unzip updates)) learningRate) (update' (biases nn) (snd (unzip updates)) learningRate)
+apply :: NeuralNetwork -> [(Matrix Float,Matrix Float)] -> Float -> NeuralNetwork
+apply nn updates learningRate = NeuralNetwork (config nn) (update (weights nn) (fst (unzip updates)) learningRate) (update (biases nn) (snd (unzip updates)) learningRate)
 
-update' :: [Matrix Float] -> [Matrix Float] -> Float -> [Matrix Float]
-update' [] _ _ = []
-update' _ [] _ = []
-update' (m:matrices) (u:updates) learningRate = m - fmap (*learningRate) u : update' matrices updates learningRate
+update :: [Matrix Float] -> [Matrix Float] -> Float -> [Matrix Float]
+update [] _ _ = []
+update _ [] _ = []
+update (m:matrices) (u:updates) learningRate = m - fmap (*learningRate) u : update matrices updates learningRate
 
 gradients :: [Matrix Float] -> [Matrix Float] -> [Matrix Float] -> Matrix Float -> [(Matrix Float, Matrix Float)]
 gradients [] _ _ _ = []
@@ -119,12 +119,13 @@ toCategorical label classes = [if i == label then 1 else 0 | i <- [0..classes-1]
 test :: NeuralNetwork -> [(Matrix Float, Matrix Float)] -> IO ()
 test _ [] = putStrLn ""
 test nn (s:samples) = do
-  let prediction = argmax (predict nn (fst s))
-  let label = argmax (snd s)
-  if prediction == label then
-    putStrLn "right"
-  else
-    putStrLn "wrong"
+                    let prediction = argmax (predict nn (fst s))
+                    let label = argmax (snd s)
+                    if prediction == label then
+                      putStrLn "right"
+                    else
+                      putStrLn "wrong"
+                    test nn samples
 
 -- Helper
 
@@ -135,7 +136,11 @@ zeroMatrix :: Int -> Int -> Matrix Float
 zeroMatrix rows columns = matrix rows columns (\(i, j) -> 0.0)
 
 multiplyElementwise :: Matrix Float -> Matrix Float -> Matrix Float
-multiplyElementwise m1 m2 = (zipWith (*) [m1] [m2])!!0
+multiplyElementwise m1 m2 = fromList (nrows m1) (ncols m2) (zipWith (*) m1List m2List)
+                              where m1List = toList m1
+                                    m2List = toList m2
+
+--multiplyElementwise m1 m2 = (zipWith (*) [m1] [m2])!!0
 
 sigmoid :: Float -> Float
 sigmoid x = 1 / (1 + exp (-x))
