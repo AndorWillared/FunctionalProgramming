d25b0e254642a4f80e6859c67cc770b5e1d12b14
Andor Kyrill Willared <andor.willared@mni.thm.de>
2019-07-08 23:32:00 +0200

    docu

--- NeuralNetwork.hs
+++ NeuralNetwork.hs
@@ -5,7 +5,7 @@ License     : MIT
 Maintainer  : andor.willared@mni.thm.de
 Stability   : experimental
 
-A naive haskell neural-network implementation
+A naive neural-network implementation in haskell.
 -}
 
 module NeuralNetwork (
@@ -23,10 +23,12 @@ module NeuralNetwork (
   update,
   gradients,
   -- * Serializiation
+  -- ** Binary coded
   serialize,
-  serialize2,
   deserialize,
-  deserialize2,
+  -- ** Plain coded
+  serializePlain,
+  deserializePlain,
   -- * Helper functions
   sigmoid,
   sigmoid',
@@ -93,16 +95,20 @@ createNeuralNetwork config = NeuralNetwork
   [zeroMatrix (config!!i) 1 | i <- [1..((length config)-1)]] 
   where range = (-1.0,1.0)
 
--- | 'predict' takes an network and a fitting input and runs a forwardPass with these parameters. 
+-- | 'predict' takes an network and a fitting input and runs a 'forwardPass' with these parameters. 
 -- The resulting output 'Matrix' is returned
-predict :: NeuralNetwork 
-        -> Matrix Float 
-        -> Matrix Float
+predict :: NeuralNetwork    -- ^ trained 'NeuralNetwork' that will be used to 'predict' an output 
+        -> Matrix Float     -- ^ vector of the input values for the given network
+        -> Matrix Float     -- ^ vector of the output node values
         
 predict nn input = last (forwardPass nn input)
 
-
-forwardPass :: NeuralNetwork -> Matrix Float -> [Matrix Float]
+-- | A function that runs one 'forwardPass' for the provided 'NeuralNetwork' 
+-- with the given input and returns the activations of all layers except the input layer.
+forwardPass :: NeuralNetwork    -- ^ trained 'NeuralNetwork' that will be used for 'forwardPass'
+            -> Matrix Float     -- ^ vector of the input values for the given network
+            -> [Matrix Float]   -- ^ list of the matrices of the activations (last one is the output vector of the network)
+            
 forwardPass nn input = input : forwardPass' (weights nn) (biases nn) input
 
 forwardPass' :: [Matrix Float] -> [Matrix Float] -> Matrix Float -> [Matrix Float]
@@ -124,11 +130,15 @@ data BackpropResult = BackpropResult { nn :: NeuralNetwork , totalError :: Float
 
 backprop :: NeuralNetwork -> Matrix Float -> Matrix Float -> Float -> Float -> Int -> IO (BackpropResult)
 backprop nn input output learningRate totalError totalIterations = do
+
   let err = 0.5 * (sum $ toList (fmap (^2) ((last activations) - output)))
   let updatedNN = apply nn (reverse (gradients (reverse (weights nn)) (reverse (biases nn)) (reverse (init activations)) ((last activations) - output))) learningRate
+ 
   putStrLn ((show (totalIterations + 1)) ++ ": " ++ show ((totalError + err)/(fromIntegral (totalIterations + 1))))
+  
   return (BackpropResult updatedNN (totalError + err) (totalIterations + 1))
-  where activations = forwardPass nn input
+  
+            where activations = forwardPass nn input
 
 apply :: NeuralNetwork -> [(Matrix Float,Matrix Float)] -> Float -> NeuralNetwork
 apply nn updates learningRate = NeuralNetwork (config nn) (update (weights nn) (fst (unzip updates)) learningRate) (update (biases nn) (snd (unzip updates)) learningRate)
@@ -155,16 +165,16 @@ deserialize path = do
   nn <- decodeFile path :: IO (NeuralNetwork)
   return nn
 
-serialize2 :: NeuralNetwork -> FilePath -> IO ()
-serialize2 nn path = do
+serializePlain :: NeuralNetwork -> FilePath -> IO ()
+serializePlain nn path = do
   writeFile path (show (
     [fromIntegral (length (config nn))]
     ++ (map fromIntegral (config nn))
     ++ (concat [ toList ((weights nn)!!i) | i <- [0..((length (config nn))-2)] ])
     ++ (concat [ toList ((biases nn)!!i) | i <- [0..((length (config nn))-2)] ])))
 
-deserialize2 :: FilePath -> IO (NeuralNetwork)
-deserialize2 path = do
+deserializePlain :: FilePath -> IO (NeuralNetwork)
+deserializePlain path = do
   input <- (readFile path)
   let flist = map read (splitOn "," (take ((length (drop 1 input)) - 1) (drop 1 input)))
   let config = map round (take (round (flist!!0)) (drop 1 flist))
---
+++ README.txt
@@ -0,0 +1,3 @@
+doku generieren mit haddock:
+
+haddock NeuralNetwork.hs -h -o C:\Users\Andor\FP\Neural-Network-Haskell\doc
